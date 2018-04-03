36440343e2b6c5a772899b81f959205fa4144760
Aria <aria@ar1as.space>
2018-04-03 09:36:23 +0800

    documentation and stuff

--- config.el
+++ config.el
@@ -1,5 +1,7 @@
 ;;;  -*- lexical-binding: t; -*-
 
+;; Bind C-/ to emmet expand because what monster uses M-e?
+;; TODO make this tab
 (map! :map emmet-mode-keymap
     :i "C-/" #'emmet-expand-yas)
 
