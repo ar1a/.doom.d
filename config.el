f10b29b99ece918ada72257b724eb0f5d63c9f88
Aria <aria@ar1as.space>
2018-04-03 10:00:10 +0800

    move emmet-expand to tab key with custom logic

--- config.el
+++ config.el
@@ -1,9 +1,31 @@
 ;;;  -*- lexical-binding: t; -*-
 
-;; Bind C-/ to emmet expand because what monster uses M-e?
-;; TODO make this tab
-(map! :map emmet-mode-keymap
-    :i "C-/" #'emmet-expand-yas)
+;; Bind TAB to emmet expand because what monster uses M-e?
+
+;; Author: hlissner
+(defun emmet-or-snippet-expand ()
+  (interactive)
+  (call-interactively
+   (cond ((and (bound-and-true-p yas-minor-mode)
+               (yas--templates-for-key-at-point))
+          #'yas-expand)
+         ((emmet-expr-on-line)
+          (if (bound-and-true-p yas-minor-mode)
+              #'emmet-expand-yas
+            #'emmet-expand-line))
+         (#'indent-for-tab-command))))
+(map! :after rjsx-mode
+      :map rjsx-mode-map
+      :i "TAB" #'emmet-or-snippet-expand)
+
+(setq emmet-expand-jsx-className? t) ;; jsx uses className not class
+
+;; Readd in rjsx's autotag shit
+(map! :after rjsx-mode
+      :map rjsx-mode-map
+      "<" #'rjsx-electric-lt
+      "C-d" #'rjsx-delete-creates-full-tag)
+
 
 (load! +ruby)
 
