545238da800353d12c5196c2505bb63aaa7e960c
Aria <aria@ar1as.space>
2018-04-05 19:02:39 +0800

    cool magit side ui thing

--- +ui.el
+++ +ui.el
@@ -11,3 +11,21 @@
 ;; Nord is pretty cool: https://github.com/arcticicestudio/nord
 ;; A nice alternative (although brighter) could be: nova - https://trevordmiller.com/projects/nova
 (setq doom-theme 'doom-nord)
+
+;; Magit rules
+;; Keeps it on the side
+;; Thanks to https://github.com/fuxialexander/doom-emacs-private-xfu
+(after! magit
+  (set! :popup "^\\(?: ?\\*\\)?magit.*: "
+    '((slot . -1) (side . right) (size . 80))
+    '((select . t) (quit . nil)))
+  (set! :popup "^\\*magit.*popup\\*"
+    '((slot . 0) (side . right))
+    '((select . t)))
+  (set! :popup "^\\(?: ?\\*\\)?magit-revision:.*"
+    '((slot . 2) (side . right) (window-height . 0.6))
+    '((select . t)))
+  (set! :popup "^\\(?: ?\\*\\)?magit-diff:.*"
+    '((slot . 2) (side . right) (window-height . 0.6))
+    '((select . nil)))
+  )
