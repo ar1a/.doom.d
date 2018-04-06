8a1723165a07fd9e7eea302569b9011f401163a3
Aria <aria@ar1as.space>
2018-04-07 00:38:59 +0800

    oops typo

--- +ui.el
+++ +ui.el
@@ -38,7 +38,7 @@
   (add-hook! 'find-file-hook
     (if (and (buffer-file-name) (neo-global--window-exists-p))
         ;; And only if the file is a child of the current neotree root
-        (if (ne-global--file-in-root-p (buffer-file-name))
+        (if (neo-global--file-in-root-p (buffer-file-name))
             ;; We need to trigger neotree-find then switch back to the buffer we just opened
             (save-current-buffer (neotree-find))
             ))
