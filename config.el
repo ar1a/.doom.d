18e2fae26b89510320130bb3eb7ec2462aca38dd
Aria <aria@ar1as.space>
2018-04-21 15:56:56 +0800

    swap back to using upstream js shit

--- config.el
+++ config.el
@@ -4,8 +4,18 @@
 (after! yasnippet
   (push (expand-file-name "snippets/" doom-private-dir) yas-snippet-dirs))
 
+(after! js2-mode
+  ;; use eslintd-fix so when i save it fixes dumb shit
+  (add-hook 'js2-mode-hook #'eslintd-fix-mode)
+  ;; Indent shit
+  (setq js2-basic-offset 2)
+  )
+
+;; Flycheck when you hit escape in insert mode not only normal mode
+(add-hook 'evil-insert-state-exit-hook #'+syntax-checkers|flycheck-buffer)
+
 ;; Modules
-(load! +web)
+;; (load! +web)
 (load! +ruby)
-(load! +lsp)
+;; (load! +lsp)
 (load! +ui)
