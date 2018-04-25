30cacf02a13d0827901a604eced412dc8b5e46ef
Aria <aria@ar1as.space>
2018-04-25 16:38:26 +0800

    format the enable-minor-mode documentation a bit nicer

--- config.el
+++ config.el
@@ -27,7 +27,8 @@
   )
 
 (defun enable-minor-mode (my-pair)
-  "Enable minor mode if filename matches the regexp. MY-PAIR is a cons cell (regexp . minor-mode)."
+  "Enable minor mode if filename matches the regexp.
+  MY-PAIR is a cons cell (regexp . minor-mode)."
   (if (buffer-file-name)
       (if (string-match (car my-pair) buffer-file-name)
           (funcall (cdr my-pair))))
