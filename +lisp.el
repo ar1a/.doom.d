cafe807a0079c6d27949b4180f39c61585a60f51
Aria <aria@ar1as.space>
2018-04-30 17:15:50 +0800

    add pretty parens to parinfer, run it in clojure mode too

--- +lisp.el
+++ +lisp.el
@@ -1,12 +1,16 @@
 ;;;  -*- lexical-binding: t; -*-
+(require 'rainbow-delimiters) ;; gotta require it because the function
+;; it calls isn't autoloaded
 
 (def-package! parinfer
   :config
   (setq parinfer-extensions
         '(defaults
            evil
+           pretty-parens
            smart-tab
            smart-yank))
   (setq parinfer-auto-switch-indent-mode t
         parinfer-auto-switch-indent-mode-when-closing t)
-  (add-hook! emacs-lisp-mode #'parinfer-mode))
+  (add-hook! emacs-lisp-mode #'parinfer-mode)
+  (add-hook! clojure-mode #'parinfer-mode))
