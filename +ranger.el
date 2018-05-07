26473eef5ee17f2c1d96e277b3ac7a2965771000
Aria <aria@ar1as.space>
2018-05-08 08:02:45 +1000

    ranger override dired not lazyloading

--- +ranger.el
+++ +ranger.el
@@ -3,11 +3,12 @@
 (def-package! ranger
   :commands (ranger deer ranger-override-dired-fn)
   :config
-  (setq-default ranger-preview-file nil) ;; The file preview is buggy for me so I disable it.
-  (add-hook! dired-mode #'ranger-override-dired-fn)) ;; Override dired-mode so it uses deer
+  (setq-default ranger-preview-file nil)) ;; The file preview is buggy for me so I disable it.
 
 (map!
  (:leader
    (:prefix "a"
     :desc "Ranger" :n "r" #'ranger
     :desc "Deer":n "d" #'deer)))
+
+(add-hook! dired-mode #'ranger-override-dired-fn) ;; Override dired-mode so it uses deer
