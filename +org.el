66156c0d5e8cbaabc892d402b2f38725062fd5f5
Aria <aria@ar1as.space>
2018-05-13 01:32:04 +1000

    Add more org binds, refiling agendas, etc

--- +org.el
+++ +org.el
@@ -5,20 +5,31 @@
 (after! org
   (map! :map evil-org-mode-map
         :localleader
-        :desc "Create/Edit Todo" :nve "t" #'org-todo
+        :desc "Create/Edit Todo" :nve "o" #'org-todo
         :desc "Schedule" :nve "s" #'org-schedule
-        :desc "Refile" :nve "r" #'org-refile))
+        :desc "Refile" :nve "r" #'org-refile
+        :desc "Filter" :nve "f" #'org-match-sparse-tree
+        :desc "Tag heading" :nve "t" #'org-set-tags-command)
 
+  ;; Move the agenda to show the previous 3 days and the next 7 days for a bit
+  ;; better context instead of just the current week which is a bit confusing
+  ;; on, for example, a sunday
+  (setq org-agenda-span 10
+        org-agenda-start-on-weekday nil
+        org-agenda-start-day "-3d"))
+
+;; org-match-sparse-tree
+;; org-set-tags-command
 (defun +open-todo-file ()
-    (interactive)
-    "Opens the todo file"
-    (find-file +todo-file))
+  (interactive)
+  "Opens the todo file"
+  (find-file +todo-file))
 
 (map!
-    (:leader
-        :desc "Open todo file" :nvm "O" #'+open-todo-file))
+ (:leader
+   :desc "Open todo file" :nvm "O" #'+open-todo-file))
 
 (map! :leader
- (:prefix "o"
-   :nvm "a" (lambda! (org-agenda nil "a"))))
+      (:prefix "o"
+        :nvm "a" (lambda! (org-agenda nil "a"))))
 
