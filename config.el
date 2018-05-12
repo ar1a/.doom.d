84d0263583f540f055b76b5e7e96f96a3ff36174
Aria <aria@ar1as.space>
2018-05-12 22:26:43 +1000

    move irc config to its own file

--- config.el
+++ config.el
@@ -21,36 +21,6 @@
 
 (after! tide
   (set! :company-backend 'tide-mode 'company-flow 'company-tide))
-
-(after! irc
-  (defun fetch-password (&rest params)
-    "Fetch a password from auth-source"
-    (require 'auth-source)
-    (let ((match (car (apply 'auth-source-search params))))
-      (if match
-          (let ((secret (plist-get match :secret)))
-            (if (functionp secret)
-                (funcall secret)
-              secret))
-        (error "Password not found for %S" params))))
-
-  (defun irc-password (server)
-    (fetch-password :user "ar1a" :host server))
-
-  ;; Freenode
-  (set! :irc "irc.freenode.net"
-    '(:use-tls t
-           :port 6697
-           :nick "ar1a"
-           :sasl-username "ar1a"
-           :sasl-password irc-password
-           :channels ("#emacs"))))
-
-
-(map! :leader
-      (:prefix "o"
-        (:nve "I" #'=irc)))
-
 (defun setup-prettier-js ()
   "Sets up arguments and the mode."
   (interactive)
@@ -135,3 +105,4 @@
 (load! +reason) ;; ReasonML stuff
 (load! +mail) ;; Mail stuff
 (load! +org) ;; Org mode stuff like todos and rebindings
+(load! +irc) ;; Irc config
