723664219eb01316e6c61f3f819e1dff3a02b31e
Aria <aria@ar1as.space>
2018-05-27 15:34:35 +1000

    My agenda changes were merged upstream

--- +org.el
+++ +org.el
@@ -15,13 +15,7 @@
   ;; Normally its only like 3 lines tall, too hard to see anything.
   (set! :popup "^\\*Org Agenda"
     '((size . 15))
-    '((transient) (quit) (select . t)))
-  ;; Move the agenda to show the previous 3 days and the next 7 days for a bit
-  ;; better context instead of just the current week which is a bit confusing
-  ;; on, for example, a sunday
-  (setq org-agenda-span 10
-        org-agenda-start-on-weekday nil
-        org-agenda-start-day "-3d"))
+    '((transient) (quit) (select . t))))
 
 ;; org-match-sparse-tree
 ;; org-set-tags-command
