bb399df51b70f7ebf016ccf0801f7dfc00bd9238
Aria <aria@ar1as.space>
2018-05-30 13:11:34 +1000

    Revert "Revert "lazy load parinfer""
    This reverts commit b0f310c0eb60fc3c1aad1dde70a830248068e862.

--- +lisp.el
+++ +lisp.el
@@ -3,16 +3,8 @@
 ;; it calls isn't autoloaded
 
 (def-package! parinfer
-  :config
-  (setq parinfer-extensions
-        '(defaults
-           evil
-           pretty-parens
-           smart-tab
-           smart-yank)
-        parinfer-auto-switch-indent-mode t
-        parinfer-auto-switch-indent-mode-when-closing t)
-
+  :commands (parinfer-mode)
+  :init
   (defun +disable-yas ()
     "Disable yasnippet so parinfer works."
     (yas-minor-mode -1)
@@ -24,6 +16,16 @@
               scheme-mode
               lisp-mode)
     #'+disable-yas)
+  :config
+  (setq parinfer-extensions
+        '(defaults
+           evil
+           pretty-parens
+           smart-tab
+           smart-yank)
+        parinfer-auto-switch-indent-mode t
+        parinfer-auto-switch-indent-mode-when-closing t)
+  ;; FIXME Parinfer isn't being mapped
   (map! :map parinfer-mode-map
         :localleader
         :nv "m" #'parinfer-toggle-mode))
