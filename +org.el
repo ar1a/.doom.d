d9180318ce42b80e57eea738bf0b4e2a6a1e8337
Aria <aria@ar1as.space>
2018-05-30 14:14:42 +1000

    Add description to SPC-o-a

--- +org.el
+++ +org.el
@@ -33,7 +33,7 @@
 
 (map! :leader
       (:prefix "o"
-        :nvm "a" (lambda! (org-agenda nil "a")))
+        :desc "Org Agenda" :nvm "a" (lambda! (org-agenda nil "a")))
       (:when (featurep! :completion helm)
         :nv "X" #'helm-org-capture-templates))
 
