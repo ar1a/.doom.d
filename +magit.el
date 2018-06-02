3c78c83fff106ccc8924911bcab4098ebe5e2f40
Aria <aria@ar1as.space>
2018-06-02 13:43:23 +1000

    Add some cool options to magit

--- +magit.el
+++ +magit.el
@@ -1,6 +1,12 @@
 ;;; ~/.doom.d/+magit.el -*- lexical-binding: t; -*-
 
 (after! magit
+  (magit-wip-after-save-mode t)
+  (magit-wip-after-apply-mode t)
+  (setq magit-save-repository-buffers 'dontask
+        magit-repository-directories '("/home/aria/projects/")
+        magit-popup-display-buffer-action nil ;; Not sure why this is here, wonder what it does
+        magit-display-file-buffer-function #'switch-to-buffer-other-window)
   ;; Magit rules
   ;; Keeps it on the side
   ;; Thanks to https://github.com/fuxialexander/doom-emacs-private-xfu
